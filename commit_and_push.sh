9b80a61664529946a4bfed15d38552dc9cb0a6ef
Pavel_Karabanovich <125821740+streamx1@users.noreply.github.com>
2023-03-07 10:13:22 +0300

    Master (#1)
    * “new1_26-02-2023__09-09”
    
    * “new2_26-02-2023__09-10”
    
    * “new4_26-02-2023__09-33”
    
    * “new4_26-02-2023__09-35”
    
    * “new4_26-02-2023__09-37”
    
    * add first action
    
    * TAB action
    
    * TAB action
    
    * TAB action
    
    * TAB action
    
    * TAB action
    
    * TAB action
    
    * TAB action
    
    * TAB action
    
    * TAB action
    
    * TAB action
    
    * TAB action
    
    * TAB action
    
    * TAB action
    
    * TAB action
    
    * TAB action
    
    * TAB action
    
    * TAB action
    
    * TAB action
    
    * tab action
    
    * tab action
    
    * tab action
    
    * tab action
    
    * tab action
    
    * tab action
    
    * tab action
    
    * tab action
    
    * tab action
    
    * add first action
    
    * TAB action
    
    * add first action
    
    * tab action_new
    
    * tab action1
    
    * tab action

---
+++ commit_and_push.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+git add --all 
+
+echo "Input commit name"
+
+read commitname
+
+git commit -m “$commitname'_'$(date '+%d-%m-%Y__%H-%M')”
+
+for local_repository in $(git remote)
+
+do 
+
+git push $local_repository --all
+
+done
+
+
